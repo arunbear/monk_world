1807e5838149e045423aba185dba0a8de3b175b4
Arun Prasaad <arunbear@cpan.org>
2025-12-02 16:10:43 +0000

    Configure secrets and logging

--- cpanfile
+++ cpanfile
@@ -2,6 +2,7 @@ requires 'perl', '5.42.0';
 requires 'Data::Dump', '1.25';
 requires 'HTTP::Message', '7.01';
 requires 'Moo', '2.005005';
+requires 'Mojolicious', '9.42';
 requires 'namespace::autoclean', '0.31';
 requires 'Role::Tiny', '2.002004';
 requires 'Type::Tiny', '2.008004';
--- lib/MonkWorld.pm
+++ lib/MonkWorld.pm
@@ -1,5 +1,8 @@
 package MonkWorld;
-use Mojo::Base 'Mojolicious', -signatures;
+
+our $VERSION = 0.001_001;
+use v5.40;
+use Mojo::Base 'Mojolicious';
 
 # This method will run once at server start
 sub startup ($self) {
@@ -8,7 +11,8 @@ sub startup ($self) {
     my $config = $self->plugin('NotYAMLConfig');
 
     # Configure the application
-    $self->secrets($config->{secrets});
+    $self->configure_logging;
+    $self->configure_secrets;
 
     # Router
     my $r = $self->routes;
@@ -17,4 +21,17 @@ sub startup ($self) {
     $r->get('/')->to('Example#welcome');
 }
 
-1;
+sub configure_logging ($self) {
+    my $config = $self->config;
+    $self->log->path($config->{logger}{path});
+    $self->log->level($config->{logger}{level});
+}
+
+sub configure_secrets ($self) {
+    if (my $secrets = $ENV{MONKWORLD_SECRETS}) {
+        $self->secrets([ split /,/ => $secrets ]);
+    }
+    else {
+        $self->log->warn('MONKWORLD_SECRETS environment variable is not set');
+    }
+}
