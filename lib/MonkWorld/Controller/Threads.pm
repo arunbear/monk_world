6bc2542ab0a616de886e052a7c44ed09208587f3
Arun Prasaad <arunbear@cpan.org>
2025-12-11 15:54:01 +0000

    User hypermedia to make the request more automated

--- lib/MonkWorld/Controller/Threads.pm
+++ lib/MonkWorld/Controller/Threads.pm
@@ -1,14 +1,20 @@
 package MonkWorld::Controller::Threads;
 use v5.40;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
-# use MonkWorld::API::Request;
+use MonkWorld::API::Request;
 use Data::Dumper;
 
 sub index ($self) {
-    my $url = $self->config->{api}{url} . '/threads';
-    my $ua = $self->app->ua;
-    my $tx = $ua->get($url);
-    my $threads = $tx->res->json;
+    my $sitemap = get_sitemap($self);
+    my $req = MonkWorld::API::Request
+        ->new(
+            link_meta => $sitemap->{_links}{get_threads},
+            server    => $self->config->{api}{url},
+        );
+
+    my $tx = $self->app->ua->build_tx($req->tx_args);
+    my $res = $self->app->ua->start($tx)->result;
+    my $threads = $res->json;
     $self->log->debug("Threads: " . Dumper($threads));
 
     $self->render(threaded => $threads, counter => make_counter());
@@ -17,6 +23,19 @@ sub index ($self) {
 sub make_counter () {
     my $count = 1;
     return sub { $count++ };
+}
+
+sub get_sitemap ($self) {
+    state $sitemap = do {
+        my $res = $self->app->ua->get($self->config->{api}{url})->result;
+        if (!$res->is_success) {
+            my $message = "Failed to fetch sitemap: " . $res->message;
+            $self->log->error($message);
+            die $message;
+        }
+        $res->json;
+    };
+    return $sitemap;
 }
 
 __END__
